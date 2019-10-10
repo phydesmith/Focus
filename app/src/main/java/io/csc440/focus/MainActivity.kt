9f48a3e813ee5e0f40443f47c6439a9eaba9e69f
peter.hydesmith <phydesmi@pio.carrollu.edu>
2019-10-10 12:55:47 -0500

    Added test comment.

--- app/src/main/java/io/csc440/focus/MainActivity.kt
+++ app/src/main/java/io/csc440/focus/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //  Test comment for source control test.
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
