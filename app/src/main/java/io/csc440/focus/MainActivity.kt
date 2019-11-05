c0aa6a5b7514a8057a5eed0f8eb976b176ea95ff
peter.hydesmith <phydesmi@pio.carrollu.edu>
2019-11-05 08:48:18 -0600

    Revert "works?"
    This reverts commit 4b89016a39f9aa409a8188e4fca74b4891424972.

--- app/src/main/java/io/csc440/focus/MainActivity.kt
+++ app/src/main/java/io/csc440/focus/MainActivity.kt
@@ -1,14 +1,11 @@
 package io.csc440.focus
 
-import android.content.Context
 import android.content.Intent
-import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,18 +20,6 @@ class MainActivity : AppCompatActivity() {
         //  Test comment for source control test.
 
 
-        //  Data base stuff
-        var created = false;
-        val database_name = "tasks"
-        val myDb = openOrCreateDatabase(database_name, Context.MODE_PRIVATE, null)
-
-
-        if (!created) {
-            myDb.execSQL("CREATE TABLE IF NOT EXISTS Tasks (description VARCHAR(100), date DATE, priority BOOLEAN);")
-            created = true;
-            myDb.close()
-        }
-
         //lateinit var focusTasks.adapter = myAdapter
         //  2nd test comment for guide.
          myAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tasks_st)
@@ -42,35 +27,16 @@ class MainActivity : AppCompatActivity() {
          //var listview:ListView = findViewById<ListView>(R.id.focusTasks)
          focusTasks?.adapter = myAdapter
 
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 
     fun newTaskClick(view: View){
         val myIntent = Intent(this, focus_creation::class.java)
         startActivityForResult(myIntent, 1)
     }
 
-
-    fun printTasks(view: View){
-        val database_name = "tasks"
-        val myDb = openOrCreateDatabase(database_name, Context.MODE_PRIVATE, null)
-        var resultSet = myDb.rawQuery("SELECT * FROM Tasks", null)
-        resultSet.moveToFirst()
-        var descriptionIndex = resultSet.getColumnIndex("description")
-        //var dateIndex = resultSet.getColumnIndex("date")
-        //var priIndex = resultSet.getColumnIndex("priority")
-        var description = resultSet.getString(descriptionIndex)
-        //var priorityOP = resultSet.getString(priIndex)
-        //var date = resultSet
-        Log.d("dataBasee", "HERE")
-        System.out.println("HERE MOFO: " + description + " PRIORITY? ")// + priorityOP)
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, myIntent: Intent?) {
-        Log.d("main", "IN MAIN")
         super.onActivityResult(requestCode, resultCode, myIntent)
         if(requestCode == task_code){
             if(myIntent != null){
