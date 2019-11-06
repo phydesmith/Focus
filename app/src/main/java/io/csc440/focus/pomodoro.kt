d796160118a8f0e2de1e9b5707eef21372ba9610
peter <peter.k.hydesmith@gmail.com>
2019-11-05 18:34:24 -0600

    added pomodoro static mock up

---
+++ app/src/main/java/io/csc440/focus/pomodoro.kt
@@ -0,0 +1,12 @@
+package io.csc440.focus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pomodoro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pomodoro)
+    }
+}
